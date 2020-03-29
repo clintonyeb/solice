844faf4672b3669e4b5f946ccc19185bb16e9120
Cinton Yeboah <clintonyeb@gmail.com>
2020-03-28 21:41:11 -0500

    Add toast component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,28 +1,27 @@
-import { SessionModule } from './session/session.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
-import { UserModule } from './user/user.module';
-import { AppComponent } from './app.component';
+import { SessionModule } from "./session/session.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"; // this is needed!
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app.routing";
+import { ComponentsModule } from "./components/components.module";
+import { UserModule } from "./user/user.module";
+import { AppComponent } from "./app.component";
+import { ToastComponent } from './components/toast/toast.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-    ],
-    imports: [
-        BrowserAnimationsModule,
-        NgbModule,
-        FormsModule,
-        RouterModule,
-        AppRoutingModule,
-        ComponentsModule,
-        SessionModule,
-        UserModule
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, ToastComponent],
+  imports: [
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+    ComponentsModule,
+    SessionModule,
+    UserModule
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
